c043fe479f532248ae5cfbb461b7098fbe916e48
Tomas Stropus <h.guzas@gmail.com>
2014-11-22 17:59:59 +0200

    Cast animation

--- Assets/Objects/Spell/Script/Cast.cs
+++ Assets/Objects/Spell/Script/Cast.cs
@@ -7,14 +7,20 @@ public class Cast : MonoBehaviour {
 	
 	public float speed = 20;
 	public float cooldown = 1;
+	public float offset = 2;
 
-	public float offset = 2;	
+	private Animator anim;
 	private float cooldown_time = 0;
+
+	void Start() {
+		anim = GetComponent<Animator>();
+	}
 
 	void Update () {
 		if (Input.GetButtonDown("Fire1") && networkView.isMine)
 		{
 			if (cooldown_time < Time.time){
+				anim.SetTrigger("Cast");
 				Rigidbody inst_fireball = Instantiate(fireball, transform.position, transform.rotation) as Rigidbody;
 
 				inst_fireball.transform.Translate(Vector3.forward * offset);
--- Assets/Objects/Spell/Script/FireballBehaviour.cs
+++ Assets/Objects/Spell/Script/FireballBehaviour.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class FireballBehaviour : MonoBehaviour {
@@ -9,6 +9,12 @@ public class FireballBehaviour : MonoBehaviour {
 
 	public AudioClip sound_splosion;
 
+	private float lastSynchronizationTime = 0f;
+	private float syncDelay = 0f;
+	private float syncTime = 0f;
+	private Vector3 syncStartPosition = Vector3.zero;
+	private Vector3 syncEndPosition = Vector3.zero;
+
 	// Use this for initialization
 	void Start () {
 		audio.Play();
@@ -17,7 +23,14 @@ public class FireballBehaviour : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (!networkView.isMine) {
+			SyncedMovement();
+		}
+	}
+
+	void SyncedMovement() {
+		syncTime += Time.deltaTime;
+		rigidbody.position = Vector3.Lerp(syncStartPosition, syncEndPosition, syncTime / syncDelay);
 	}
 
 	void OnCollisionEnter (Collision col){
@@ -26,14 +39,38 @@ public class FireballBehaviour : MonoBehaviour {
 			Destroy(gameObject, 0.1f);
 		}
 		else{
-			col.rigidbody.AddForce(this.rigidbody.velocity.normalized*knockback);
+			col.rigidbody.AddForce(this.rigidbody.velocity.normalized * knockback);
 		}
 		if(col.collider.tag == "Fragile"){
 			col.gameObject.GetComponent<HealthMeter>().DoDamage(Random.Range(damage_from, damage_to));
 			Destroy(gameObject);
 		}
 	}
-	void OnDestroy(){
+
+	void OnSerializeNetworkView(BitStream stream, NetworkMessageInfo info)
+	{
+		Vector3 syncPosition = Vector3.zero;
+		Vector3 syncVelocity = Vector3.zero;
+		if (stream.isWriting)
+		{
+			syncPosition = rigidbody.position;
+			stream.Serialize(ref syncPosition);
+			
+			syncVelocity = rigidbody.velocity;
+			stream.Serialize(ref syncVelocity);
+		}
+		else
+		{
+			stream.Serialize(ref syncPosition);
+			stream.Serialize(ref syncVelocity);
+			
+			syncTime = 0f;
+			syncDelay = Time.time - lastSynchronizationTime;
+			lastSynchronizationTime = Time.time;
+			
+			syncEndPosition = syncPosition + syncVelocity * syncDelay;
+			syncStartPosition = rigidbody.position;
+		}
 	}
 
 }
