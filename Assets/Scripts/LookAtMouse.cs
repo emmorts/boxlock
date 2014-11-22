bce50196d50a51713734743a7aa8b279b305afdc
Tomas Stropus <h.guzas@gmail.com>
2014-11-22 14:37:59 +0200

    Walking with animations

--- Assets/Scripts/LookAtMouse.cs
+++ Assets/Scripts/LookAtMouse.cs
@@ -13,17 +13,6 @@ public class LookAtMouse : MonoBehaviour
 	
 	// Update is called once per frame
 	void Update () {
-        // create a ray from the mousePosition
-		var mousePos = Input.mousePosition;
-		Debug.Log(mousePos);
-		var pointTo = new Vector3 (0f, mousePos.y, 0f);
-		var ray = Camera.main.ScreenPointToRay(mousePos);
-        // plane.Raycast returns the distance from the ray start to the hit point
-	    RaycastHit floorHit;
-
-	    if (Physics.Raycast(ray, out floorHit))
-	    {
-            transform.LookAt(floorHit.point);
-	    }
+		transform.RotateToMouse (25);
 	}
 }
