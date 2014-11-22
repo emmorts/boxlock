8f81eac87c49f244370869c5c7c0f080a3990799
Tomas Stropus <h.guzas@gmail.com>
2014-11-22 18:39:17 +0200

    Improved casting

--- Assets/Objects/Spell/Script/Cast.cs
+++ Assets/Objects/Spell/Script/Cast.cs
@@ -21,11 +21,11 @@ public class Cast : MonoBehaviour {
 		{
 			if (cooldown_time < Time.time){
 				anim.SetTrigger("Cast");
-				Rigidbody inst_fireball = Instantiate(fireball, transform.position, transform.rotation) as Rigidbody;
+				Rigidbody inst_fireball = Network.Instantiate(fireball, transform.position, transform.rotation, 0) as Rigidbody;
 
 				inst_fireball.transform.Translate(Vector3.forward * offset);
 
-				inst_fireball.AddForce(GetComponentInParent<Transform>().forward.normalized*speed);
+				inst_fireball.AddForce(GetComponentInParent<Transform>().forward.normalized * speed);
 				cooldown_time = Time.time + cooldown;
 			}
 		}
--- Assets/Objects/Spell/Script/FireballBehaviour.cs
+++ Assets/Objects/Spell/Script/FireballBehaviour.cs
@@ -7,7 +7,7 @@ public class FireballBehaviour : MonoBehaviour {
 	public int damage_from = 0;
 	public int damage_to = 0;
 
-	public AudioClip sound_splosion;
+	public AudioClip explosionSound;
 
 	private float lastSynchronizationTime = 0f;
 	private float syncDelay = 0f;
@@ -34,14 +34,13 @@ public class FireballBehaviour : MonoBehaviour {
 	}
 
 	void OnCollisionEnter (Collision col){
-		AudioSource.PlayClipAtPoint(sound_splosion, this.transform.position);
-		if(col.collider.tag == "Fireball" || col.collider.tag == "Destruction"){
+		AudioSource.PlayClipAtPoint(explosionSound, this.transform.position);
+		if (col.collider.tag == "Fireball" || col.collider.tag == "Destruction"){
 			Destroy(gameObject, 0.1f);
-		}
-		else{
+		} else {
 			col.rigidbody.AddForce(this.rigidbody.velocity.normalized * knockback);
 		}
-		if(col.collider.tag == "Fragile"){
+		if (col.collider.tag == "Fragile"){
 			col.gameObject.GetComponent<HealthMeter>().DoDamage(Random.Range(damage_from, damage_to));
 			Destroy(gameObject);
 		}
