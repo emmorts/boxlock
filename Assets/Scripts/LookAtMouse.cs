4cef6702beb200b30b4101471ecf093076681943
Tomas Stropus <h.guzas@gmail.com>
2014-11-22 15:55:39 +0200

    Updates to multiplayer

--- Assets/Scripts/LookAtMouse.cs
+++ Assets/Scripts/LookAtMouse.cs
@@ -7,12 +7,13 @@ public class LookAtMouse : MonoBehaviour
 	// Use this for initialization
     void Start()
     {
-        if (rigidbody)
+        if (rigidbody && networkView.isMine) 
             rigidbody.freezeRotation = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        if (networkView.isMine)
 		transform.RotateToMouse (25);
 	}
 }
