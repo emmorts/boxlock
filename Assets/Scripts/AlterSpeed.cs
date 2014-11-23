dcb0336c46b5728890a9c0ecd3b2d83d9e6c3f18
Tomas Stropus <h.guzas@gmail.com>
2014-11-23 18:49:42 +0200

    Sandbox now slows down

---
+++ Assets/Scripts/AlterSpeed.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlterSpeed : MonoBehaviour {
+
+	public float speedCoefficient = 1.0f;
+
+	private float originalSpeed = 1.0f;
+
+	void OnCollisionEnter (Collision col)
+	{
+		if (col.collider.tag == "Player") {
+			originalSpeed = col.gameObject.GetComponent<MainCharacterController> ().speed;
+		}
+	}
+	
+	void OnCollisionStay (Collision col)
+	{
+		if (col.collider.tag == "Player") {
+			col.gameObject.GetComponent<MainCharacterController>().speed = originalSpeed * speedCoefficient;
+		}
+	}
+
+	void OnCollisionExit (Collision col)
+	{
+		if (col.collider.tag == "Player") {
+			col.gameObject.GetComponent<MainCharacterController> ().speed = originalSpeed;
+		}
+	}
+}
