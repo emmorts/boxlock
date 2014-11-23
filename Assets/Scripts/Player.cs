aa267a3a20bdf1a5bd2f6d37edf28f0c34683dbd
Tomas Stropus <h.guzas@gmail.com>
2014-11-23 19:52:25 +0200

    Respawning

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,38 +3,54 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 	public float health = 100;
-	public float armor_value= 1;
-	public float h_regen = 1;
+	public float armorValue = 1;
+	public float healthRegen = 1;
+	public int timeToRespawn = 5;
 
-	float time_to_wait_for_regen = 5;
-	float time_until_regen = 0;
+	private Animator animator;
+	private float timeToWaitUntilRegen = 5;
+	private float timeUntilRegen = 0;
 
-	float damage_from_armor(float damage){
-		float armor = 1-(armor_value/100);
-		return damage*armor;
+	float DamageFromArmor (float damage){
+		float armor = 1 - (armorValue / 100);
+		return damage * armor;
 	}
 
-	public void DoDamage(float damage){
-		health -= damage_from_armor(damage);
+	public void DoDamage (float damage){
+		health -= DamageFromArmor(damage);
 	}
 
-	void DoRegen(){
-		if(health < 100 && (time_until_regen < Time.time)){
-			time_until_regen = time_to_wait_for_regen + Time.time;
-			health+=h_regen;
+	void DoRegen (){
+		if(health < 100 && (timeUntilRegen < Time.time)){
+			timeUntilRegen = timeToWaitUntilRegen + Time.time;
+			health += healthRegen;
 		}
 	}
 
 	void Start () {
-
+		animator = gameObject.GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(health <= 0){
-			Destroy(gameObject);
+		if (health <= 0){
+			gameObject.GetComponent<MainCharacterController>().enabled = false;
+			gameObject.GetComponent<LookAtMouse>().enabled = false;
+			gameObject.GetComponent<Cast>().enabled = false;
+			animator.SetBool("IsFallen", true);
+			StartCoroutine(Respawn(timeToRespawn));
 		}
 		DoRegen();
+	}
+
+	IEnumerator Respawn(int timeToRespawn = 0)
+	{
+		yield return new WaitForSeconds (timeToRespawn);
+		animator.SetBool ("IsFallen", false);
+		gameObject.GetComponent<MainCharacterController>().enabled = true;
+		gameObject.GetComponent<LookAtMouse>().enabled = true;
+		gameObject.GetComponent<Cast>().enabled = true;
+		health = 100;
 	}
 
 }
